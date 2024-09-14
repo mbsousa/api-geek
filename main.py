23680a32492faf052429a953d642ea308136c42a
Mirelle <mirellebeatriz7@gmail.com>
2024-09-14 00:07:37 -0300

    Adicionado pull e algumas especificações

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from fastapi import APIRouter, FastAPI, HTTPException
+from fastapi import APIRouter, FastAPI, HTTPException, Query
 from dtos import RequisicaoProduto
 from models import Produto
 from product_service import ProdutosService
+from typing import List,Optional
 
 app = FastAPI()
 router = APIRouter()
 produto_service = ProdutosService()
 
+app.include_router(router, prefix="/api/geek")
+
+# Create
 @router.post("/produtos/", response_model=Produto)
-def create_produto(produto: RequisicaoProduto, qt_estoque: int = 0):
+def create_produto(produto: RequisicaoProduto, qt_estoque: int):
     db_produto = Produto(
         nome=produto.nome,
         descricao=produto.descricao,
@@ -19,6 +23,7 @@ def create_produto(produto: RequisicaoProduto, qt_estoque: int = 0):
     )
     return produto_service.save_produto(db_produto)
 
+# Detail
 @router.get("/produtos/{id}", response_model=Produto)
 def produto_detail(id: int):
     produto = produto_service.get_produto_by_id(id)
@@ -26,8 +31,29 @@ def produto_detail(id: int):
         raise HTTPException(status_code=404, detail="Produto não encontrado")
     return produto
 
+""" Permitir a filtragem de produtos por nome, preço, categoria ou franquia. """
+#List
 @router.get("/produtos/", response_model=list[Produto])
-def list_produtos(preco: float = None):
-    return produto_service.get_all_produtos(preco)
+def list_produtos(
+    nome: Optional[str] = Query(None, description="Nome do produto"),
+    preco_min: Optional[float] = Query(None, description="Preço mínimo"),
+    preco_max: Optional[float] = Query(None, description="Preço máximo"),
+    categoria: Optional[str] = Query(None, description="Categoria do produto"),
+    franquia: Optional[str] = Query(None, description="Franquia")
+):  
+    return produto_service.get_all_produtos( 
+        nome=nome,
+        preco_min=preco_min,
+        preco_max=preco_max,
+        categoria=categoria,
+        franquia=franquia)
+    
 
-app.include_router(router, prefix="/api/geek")
+#Update estoque
+@router.put("/produtos/{produto_id}/att/")
+def selecionar_produto(produto_id: int, qt_estoque: Optional[int] = None):
+    try:
+        produto = produto_service.get_and_update_produto(produto_id, qt_estoque)
+        return {"message": "Produto selecionado com sucesso", "produto": produto}
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
--- product_service.py
+++ product_service.py
@@ -1,8 +1,10 @@
 # Lógica de negócio e interação com o banco de dados
 
 from sqlmodel import Session, select
-from database import get_engine, SessionLocal
+from database import get_engine
 from models import Produto
+from typing import Optional
+from sqlalchemy import update  
 
 class ProdutosService:
     def __init__(self):
@@ -13,15 +15,48 @@ class ProdutosService:
             s = select(Produto).where(Produto.id == id)
             return session.exec(s).one_or_none()
     
-    def get_all_produtos(self, preco: float):
+    def get_all_produtos(self, nome: Optional[str] = None, preco_min: Optional[float] = None, preco_max: Optional[float] = None, categoria: Optional[str] = None, franquia: Optional[str] = None):
         with Session(self.engine) as session:
             s = select(Produto).where(Produto.qt_estoque > 0)
-            if preco is not None:
-                if preco < 0:
-                    raise ValueError("O preço não pode ser negativo.")
-                s = s.where(Produto.preco == preco)
-            return session.exec(s).all()
+
+        if nome:
+            s = s.where(Produto.nome == nome)
         
+        if preco_min is not None:
+            if preco_min < 0:
+                raise ValueError("O preço mínimo não pode ser negativo.")
+            s = s.where(Produto.preco >= preco_min)
+
+        if preco_max is not None:
+            if preco_max < 0:
+                raise ValueError("O preço máximo não pode ser negativo.")
+            s = s.where(Produto.preco <= preco_max)
+
+        if categoria:
+            s = s.where(Produto.categoria == categoria)
+
+        if franquia:
+            s = s.where(Produto.franquia == franquia)
+
+        return session.exec(s).all()
+    
+    def get_and_update_produto(self,produto_id:int, qt_estoque: Optional[int] = None):
+        with Session(self.engine) as session:
+            produto = session.exec(select(Produto).where(Produto.id == produto_id)).one_or_none()
+
+        if produto is None:
+            raise ValueError("Produto não encontrado.")
+        
+        if produto.qt_estoque <= 0:
+            raise ValueError("Produto indisponível.")
+        
+        if produto.qt_estoque > 0:
+            updated_qt_estoque = produto.qt_estoque - 1
+            session.execute(update(Produto).where(Produto.id == produto_id).values(qt_estoque=updated_qt_estoque))
+            session.commit()
+        
+        return produto
+
     def save_produto(self, produto: Produto):
         try:
             with Session(self.engine) as session:
